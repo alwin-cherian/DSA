dcb9278063200ea180d4e604eff15407b93bd223
alwin-cherian <57612552+alwin-cherian@users.noreply.github.com>
2025-10-20 16:53:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.8 MB (74.34%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int SmallestEvenMultiple(int n) {
+
+        //sample added
+        
+        if(n%2 ==0){
+            return n;
+        }else{
+            return n * 2;
+        }
+    }
+}
